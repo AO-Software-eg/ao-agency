00e9f79551ca51a4f43ac933dd5f34e1fce886b0
som3a-dev <omar.eltayeb54@gmail.com>
2026-09-05 22:05:24 +0300

    performance: lazy load heavier components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
+
+import dynamic from "next/dynamic";
+
 import Hero from "../components/sections/Hero";
 import InfoCard from "../components/sections/InfoCard";
-import { Features } from "@/components/sections/Features";
-import Projects from "@/components/projects/Projects";
 import Contact from "../components/sections/Contact";
-import OurProjects from "@/components/sections/OurProjects";
 import TechStack from "@/components/sections/TechStack";
+
+const Features = dynamic(() => import("@/components/sections/Features"));
+const Projects = dynamic(() => import("@/components/projects/Projects"));
+const OurProjects = dynamic(() => import("@/components/sections/OurProjects"));
+
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center gap-10" >
+    <div className="min-h-screen w-full flex flex-col items-center justify-center gap-10">
       <Hero />
       <InfoCard />
       <Features />
